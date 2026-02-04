35edd623930e157478d3bea50f3aeec6619e9d02
kokic <3250679033@qq.com>
2026-02-05 00:08:49 +0800

    chore: block - create with classic

--- dist/js/world-renderer.js
+++ dist/js/world-renderer.js
@@ -1016,7 +1016,7 @@ function renderTestChunk({
       `| Biome: ${biomeName} ` +
       `| Loaded: ${chunkDatas.size} ` +
       `| Meshes: ${chunkMeshes.size} ` +
-      ` (8:48)`;
+      ` (11:56)`;
     requestAnimationFrame(draw);
   }
 
